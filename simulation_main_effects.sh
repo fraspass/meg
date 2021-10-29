82fd8a2f0cb99a7b71fa42b764a1cbc9a0b7509e
fraspass <fraspass@outlook.com>
2021-10-29 12:11:59 +0100

    simulate main effects

---
+++ simulation_main_effects.sh
@@ -0,0 +1,11 @@
+## Simulate fully connected MEGs with n=2 nodes (interaction terms only)
+./simulation_main_effects.py -S 117 &
+./simulation_main_effects.py -S 111 &
+./simulation_main_effects.py -S 661 &
+./simulation_main_effects.py -S 166 &
+./simulation_main_effects.py -S 777 &
+./simulation_main_effects.py -S 414 &
+./simulation_main_effects.py -S 141 &
+./simulation_main_effects.py -S 447 &
+./simulation_main_effects.py -S 744 &
+./simulation_main_effects.py -S 999 &
