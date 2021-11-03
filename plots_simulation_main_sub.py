7a51be937c9ff58c941ae15067f53b1fb81041d5
fraspass <fraspass@outlook.com>
2021-11-03 00:58:33 +0000

    added plots

--- plots_simulation_main_sub.py
+++ plots_simulation_main_sub.py
@@ -6,6 +6,15 @@ from matplotlib import rc
 rc('font',**{'family':'serif','serif':['Times']})
 rc('text', usetex=True)
 
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("-M", type=int, dest="M", default=250, const=True, nargs="?",\
+	help="Number of events used for inference. Default M=250.")
+
+## Parse arguments
+args = parser.parse_args()
+M = args.M
+
 ## Truth
 alpha = np.array([0.01,0.05]); beta = np.array([0.07,0.03]); baseline = np.add.outer(alpha,beta)
 mu = np.array([0.2,0.15]); mu_prime = np.array([0.1,0.25])
@@ -13,8 +22,8 @@ phi = np.array([0.8,0.85]); phi_prime = np.array([0.9,0.75])
 decay = mu + phi; decay_prime = mu_prime + phi_prime
 
 ## Estimates of baseline
-alpha_em = np.load('simulation_main/alpha_em.npy'); beta_em = np.load('simulation_main/beta_em.npy')
-alpha_ga = np.load('simulation_main/alpha_ga.npy'); beta_ga = np.load('simulation_main/beta_ga.npy') 
+alpha_em = np.load('simulation_main/estimation_'+str(M)+'/alpha_em.npy'); beta_em = np.load('simulation_main/estimation_'+str(M)+'/beta_em.npy')
+alpha_ga = np.load('simulation_main/estimation_'+str(M)+'/alpha_ga.npy'); beta_ga = np.load('simulation_main/estimation_'+str(M)+'/beta_ga.npy') 
 baseline_em = np.array([np.add.outer(alpha_em[i], beta_em[i]) for i in range(alpha_em.shape[0])])
 baseline_ga = np.array([np.add.outer(alpha_ga[i], beta_ga[i]) for i in range(alpha_ga.shape[0])])
 
@@ -36,12 +45,12 @@ for i in [0,1]:
         axs[i,j].set_ylabel('Density')
         axs[i,j].set_xlabel('$\\hat{\\alpha}_{'+str(i+1)+'}+\\hat{\\beta}_{'+str(j+1)+'}$')
 
-plt.savefig("simulation_main/alpha_beta.png", bbox_inches='tight', pad_inches = 0.1, dpi=500)
+plt.savefig('simulation_main/estimation_'+str(M)+'/alpha_beta.png', bbox_inches='tight', pad_inches = 0.1, dpi=500)
 plt.show(block=False)
 
 ## Estimates of jump
-mu_em = np.load('simulation_main/mu_em.npy'); mu_prime_em = np.load('simulation_main/mu_prime_em.npy')
-mu_ga = np.load('simulation_main/mu_ga.npy'); mu_prime_ga = np.load('simulation_main/mu_prime_ga.npy') 
+mu_em = np.load('simulation_main/estimation_'+str(M)+'/mu_em.npy'); mu_prime_em = np.load('simulation_main/estimation_'+str(M)+'/mu_prime_em.npy')
+mu_ga = np.load('simulation_main/estimation_'+str(M)+'/mu_ga.npy'); mu_prime_ga = np.load('simulation_main/estimation_'+str(M)+'/mu_prime_ga.npy') 
 
 fig, axs = plt.subplots(2, 2, constrained_layout=True)
 for i in [0,1]:
@@ -77,12 +86,12 @@ for i in [0,1]:
             axs[i,j].set_ylabel('Density')
             axs[i,j].set_xlabel('$\\hat{\\mu}^\\prime_{'+str(i+1)+'}$')
 
-plt.savefig("simulation_main/mu.png", bbox_inches='tight', pad_inches = 0.1, dpi=500)
+plt.savefig('simulation_main/estimation_'+str(M)+'/mu.png', bbox_inches='tight', pad_inches = 0.1, dpi=500)
 plt.show(block=False)
 
 ## Estimates of decay
-phi_em = np.load('simulation_main/phi_em.npy'); phi_prime_em = np.load('simulation_main/phi_prime_em.npy')
-phi_ga = np.load('simulation_main/phi_ga.npy'); phi_prime_ga = np.load('simulation_main/phi_prime_ga.npy') 
+phi_em = np.load('simulation_main/estimation_'+str(M)+'/phi_em.npy'); phi_prime_em = np.load('simulation_main/estimation_'+str(M)+'/phi_prime_em.npy')
+phi_ga = np.load('simulation_main/estimation_'+str(M)+'/phi_ga.npy'); phi_prime_ga = np.load('simulation_main/estimation_'+str(M)+'/phi_prime_ga.npy') 
 decay_em = mu_em + phi_em; decay_prime_em = mu_prime_em + phi_prime_em
 decay_ga = mu_ga + phi_ga; decay_prime_ga = mu_prime_ga + phi_prime_ga
 
@@ -120,12 +129,36 @@ for i in [0,1]:
             axs[i,j].set_ylabel('Density')
             axs[i,j].set_xlabel('$\\hat{\\mu}^\\prime_{'+str(i+1)+'}+\\hat{\\phi}^\\prime_{'+str(i+1)+'}$')
 
-plt.savefig("simulation_main/mu_phi.png", bbox_inches='tight', pad_inches = 0.1, dpi=500)
+plt.savefig('simulation_main/estimation_'+str(M)+'/mu_phi.png', bbox_inches='tight', pad_inches = 0.1, dpi=500)
+plt.show(block=False)
+
+## Plots for asymptotic comparisons
+fig, axs = plt.subplots(2, 1, figsize=(3.2,4.8), constrained_layout=True)
+for i in [0,1]:
+    mmin = np.min([np.min(decay_em[:,i]),np.min(decay_ga[:,i])])
+    mmax = np.max([np.max(decay_em[:,i]),np.max(decay_ga[:,i])])
+    positions = np.linspace(mmin,mmax,num=250)
+    kernel_em = kde(decay_em[:,i], bw_method='silverman')
+    kernel_ga = kde(decay_ga[:,i], bw_method='silverman')
+    axs[i].hist(decay_em[:,i], density=True, bins=15, color='lightgray', histtype=u'step', lw=2)
+    axs[i].hist(decay_ga[:,i], density=True, bins=15, color='lightgray', histtype=u'step', ls='dashed', lw=2)
+    axs[i].plot(positions, kernel_em(positions), lw=3, label='EM')
+    axs[i].plot(positions, kernel_ga(positions), ls='dashed', lw=3, label='Adam')
+    axs[i].axvline(x=decay[i],ls='dotted',c='black', lw=3, label='Truth')
+    if i == 0:
+        axs[i].legend()
+        axs[i].set_xlim(0,2.75)
+    else:
+        axs[i].set_xlim(0,3.75)
+    axs[i].set_ylabel('Density')
+    axs[i].set_xlabel('$\\hat{\\mu}_{'+str(i+1)+'}+\\hat{\\phi}_{'+str(i+1)+'}$')
+
+plt.savefig('simulation_main/estimation_'+str(M)+'/mu_phi_asy_'+str(M)+'.png', bbox_inches='tight', pad_inches = 0.1, dpi=500)
 plt.show(block=False)
 
 # Import KS scores
-ks_em = np.load('simulation_main/ks_score_em.npy')
-ks_ga = np.load('simulation_main/ks_score_ga.npy')
+ks_em = np.load('simulation_main/estimation_'+str(M)+'/ks_score_em.npy')
+ks_ga = np.load('simulation_main/estimation_'+str(M)+'/ks_score_ga.npy')
 fig, axes = plt.subplots()
 bplot = axes.boxplot([ks_ga,ks_em],vert=False,labels=['Adam', 'EM'], widths=.5, patch_artist=True)
 for element in ['boxes', 'whiskers', 'fliers', 'means', 'medians', 'caps']:
@@ -138,5 +171,22 @@ colors = ['orange', 'cornflowerblue']
 for patch, color in zip(bplot['boxes'], colors):
     patch.set_facecolor(color)
 
-plt.savefig("simulation_main/ks_scores.png", bbox_inches='tight', pad_inches = 0.1, dpi=500)
+plt.savefig('simulation_main/estimation_'+str(M)+'/ks_scores.png', bbox_inches='tight', pad_inches = 0.1, dpi=500)
+plt.show(block=False)
+
+## Vertical boxplot
+fig, axes = plt.subplots(figsize=(3.2,2.4))
+bplot = axes.boxplot([ks_em,ks_ga],vert=True,labels=['EM','Adam'], widths=.5, patch_artist=True)
+for element in ['boxes', 'whiskers', 'fliers', 'means', 'medians', 'caps']:
+    plt.setp(bplot[element], color='black')
+
+axes.yaxis.grid(True)
+axes.set_ylabel('KS scores')
+axes.set_ylim(0,0.0875)
+# Fill with colors
+colors = ['cornflowerblue','orange']
+for patch, color in zip(bplot['boxes'], colors):
+    patch.set_facecolor(color)
+
+plt.savefig('simulation_main/estimation_'+str(M)+'/ks_scores_vert_'+str(M)+'.png', bbox_inches='tight', pad_inches = 0.1, dpi=500)
 plt.show(block=False)
