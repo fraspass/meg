db59fa1627ad33d4735cf6e30b2be18845378ebd
fraspass <fraspass@outlook.com>
2021-11-03 13:50:48 +0000

    simulations erdos

--- estimate_simulation_erdos.sh
+++ estimate_simulation_erdos.sh
@@ -1,5 +1,5 @@
 ## Simulation 1
-./estimate_simulation_erdos.py -f simulation_1 -M 5000 -p 0.25 -n 10 -d 1 -m -i -hm -hi & 
+./estimate_simulation_erdos.py -f simulation_1 -eta 1e-2 -d 1 -m -i -hm -hi & 
 
 ## Simulation 2
 ./estimate_simulation_erdos.py -f simulation_2 -eta 1e-3 -d 5 -i &
