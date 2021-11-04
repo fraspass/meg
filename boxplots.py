b81cd7143cf0621c5f6d0b83a7e74ce806571595
fraspass <fraspass@outlook.com>
2021-11-04 14:38:08 +0000

    boxplots

--- boxplots.py
+++ boxplots.py
@@ -6,12 +6,14 @@ rc('font',**{'family':'serif','serif':['Times']})
 rc('text', usetex=True)
 
 # Random test data
-y = np.loadtxt('simulation_1/ks_scores.csv')
-y2 = np.loadtxt('simulation_2/ks_scores.csv')
+y = np.load('simulation_1/ks_scores.npy')
+y2 = np.load('simulation_2/ks_scores.npy')
 
 fig, axes = plt.subplots(figsize=(7,3))
 
-bplot2 = axes.boxplot([y2,y],vert=False,labels=['Simulation 2', 'Simulation 1'], widths=.5, notch=True, patch_artist=True)
+bplot2 = axes.boxplot([y2,y],vert=False,labels=['Simulation 2', 'Simulation 1'], widths=.5, notch=False, patch_artist=True)
+for element in ['boxes', 'whiskers', 'fliers', 'means', 'medians', 'caps']:
+    plt.setp(bplot2[element], color='black')
 
 # fill with colors
 colors = ['lightblue', 'lightgreen']
@@ -21,5 +23,5 @@ for patch, color in zip(bplot2['boxes'], colors):
 axes.xaxis.grid(True)
 axes.set_xlabel('Kolmogorov-Smirnov scores')
 
-plt.savefig("boxplots_simulation.pdf",bbox_inches='tight',pad_inches = 0.1)
-plt.show()
+plt.savefig('boxplots_simulation.pdf',bbox_inches='tight',pad_inches = 0.1)
+plt.show(block=False)
