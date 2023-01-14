ac804acabf0e669866f456fd6f26488e298d0266
fraspass <fraspass@outlook.com>
2023-01-14 02:56:02 +0000

    fixed enron file

--- scripts/enron_filter.sh
+++ scripts/enron_filter.sh
@@ -1,1 +1,2 @@
-curl http://www.cis.jhu.edu/~parky/Enron/execs.email.linesnum | sort -u | awk -F ' ' '$2 != $3 && $1 > 315522000 {print}' | tr ' ' ',' | sort -n -k 1 -t ',' > ../data/enron_tuples_filter.txt
+curl https://www.cis.jhu.edu/~parky/Enron/execs.email.linesnum | sort -u | 
+awk -F ' ' '$2 != $3 && $1 > 315522000 {print}' | tr ' ' ',' | sort -n -k 1 -t ',' > ../data/enron_tuples_filter.txt
